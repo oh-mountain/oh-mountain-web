afc7293c770b6f613689847aed7b5703d5eca3c1
Yutaka Sano <sano.yutaka@gmail.com>
2018-05-23 13:56:36 +0900

    use Vue.js

---
+++ src/components/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <!--PC版-->
+    <TopPc v-if="!isMobile()"/>
+    <!--SP版-->
+    <TopSp v-if="isMobile()"/>
+  </div>
+</template>
+
+<script>
+import detectBrowser from 'detect-browser'
+import TopPc from './pc/TopPc'
+import TopSp from './sp/TopSp'
+
+const mixinDetictingMobile = {
+  methods: {
+    isMobile: () => {
+      const browser = detectBrowser.detect()
+      return (browser.os.indexOf('Android') > -1 || browser.os.indexOf('iOS') > -1)
+    }
+  }
+}
+
+export default {
+  components: {TopSp, TopPc},
+  mixins: [mixinDetictingMobile],
+  name: 'Top',
+  created () {
+    console.log('loaded Index')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/sp/TopSp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>TopSp</div>
+</template>
+
+<script>
+export default {
+  name: 'TopSp'
+}
+</script>
+
+<style scoped>
+
+</style>
